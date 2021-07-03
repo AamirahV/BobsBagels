6cd58770006dc7defa38314eb127d90373097c9e
Aamirah <aamz_333@hotmail.com>
2021-07-03 16:28:01 +0100

    update

--- spec/basket.spec.js
+++ spec/basket.spec.js
@@ -1,41 +1,98 @@
 const assertEquals =  require("../src/assertEquals.js");
 const Basket = require("../src/basket.js");
 
-let basket, result
 
+let basket, input, output, total
+
+//Test 1
+console.log("Test to see if 1 item is added")
 //setup 1 item
 basket = new Basket ()
-
 //execute 1 item
-result = basket.add("Banana Bagel")
-
+total = basket.add(["Banana Bagel"])
+output = ["Banana Bagel"]
 //verify 1 item
 console.log("Can add one item")
-console.log(assertEquals(result.length, 1))
-console.log(assertEquals(result.includes("Banana Bagel"), true))
+console.log(assertEquals(total.length, 1))
+console.log(assertEquals(total,  output))
 
-
+//Test 2
+console.log("Test to see if 2 items are added")
 //setup 2 items
 basket = new Basket ()
-
 //execute 2 items
-result1 = basket.add("Banana Bagel")
-result2 = basket.add("Blueberry Bagel")
-
+total1 = basket.add(["Banana Bagel"])
+total2 = basket.add(["Blueberry Bagel"])
+output = ["Banana Bagel", "Blueberry Bagel"]
 //verify 2 items
 console.log("Can add two items")
-console.log(assertEquals(result2.length, 2))
-console.log(assertEquals(result1.includes("Banana Bagel"), true))
-console.log(assertEquals(result2.includes("Blueberry Bagel"), true))
+console.log(assertEquals(total2.length, 2))
+console.log(assertEquals(total1, output))
+console.log(assertEquals(total2, output))
 
+//Test 3
+console.log("Test to see if item is removed")
 //setup remove item
 basket = new Basket ()
-basket.add("Banana Bagel")
-
+basket.add(["Banana Bagel"])
 //execute remove item
-result = basket.remove("Banana Bagel")
-
+total = basket.remove(["Banana Bagel"])
+output = []
 //verify remove item
 console.log("remove item")
-console.log(assertEquals(result.length, 0))
-console.log(assertEquals(result.includes("Banana Bagel"), false))
+console.log(assertEquals(total.length, 0))
+console.log(assertEquals(total, output))
+
+//Test 4
+console.log("Test to see if basket is full")
+//setup basket capacity
+basket = new Basket(2)
+input = ["Bagel A", "Bagel B", "Bagel C"]
+output= ["Bagel A", "Bagel B"]
+//execute basket capacity
+total = basket.add(input)
+console.log(total)
+//verify basket capacity
+console.log("basket full")
+console.log(assertEquals(total, output))
+
+//Test 5
+console.log("Test to see if basket capacity has increased")
+//setup increased capacity
+basket = new Basket(10)
+input = 5
+output = 15
+//execute increased capacity
+total = basket.capacity
+//console.log(total)
+//verify increased capacity
+console.log("Basket capacity has increased")
+console.log(assertEquals(basket.increaseCapacity(input), output))
+
+
+//Test 6
+console.log("Test to see if item has already been added to basket")
+//setup already added Bagel
+basket = new Basket(3)
+input = basket.add(["Bagel A", "Bagel B", "Bagel C"])
+output = "Bagel A is already in the basket"
+//execute already added Bagel
+total = basket.add(["Bagel A"])
+//verify already added Bagel
+// console.log("Item already added")
+console.log("Item is already in the basket")
+console.log(assertEquals(total, output))
+
+
+//Test 7
+console.log("Test to see if an item can be removed if it's not in the basket")
+//setup already added Bagel
+basket = new Basket(3)
+input = basket.add(["Bagel A", "Bagel B", "Bagel C"])
+output = "Bagel D cannot be removed, it is not in the basket"
+//execute already added Bagel
+total = basket.remove(["Bagel D"])
+//verify already added Bagel
+// console.log("Item already added")
+console.log("Item cannot be removed, it is not in the basket")
+console.log(assertEquals(total, output))
--- src/basket.js
+++ src/basket.js
@@ -1,21 +1,75 @@
 class Basket {
-    constructor() {
+    constructor(capacity) {
         this.items = [];
+        this.capacity = capacity;
+        this.total = 0;
+        //this.list = [];
+        
     }
-    add(item) {
-        // insert item into items array
-        this.items.push(item);
+    add(bagel) {
+        for (let i = 0; i < bagel.length; i++) {
+            if (this.compareToList(bagel[i])) { 
+                   return `${bagel[i]} is already in the basket`
+            }
+            if (this.basketCapacity()) {
+                return this.items
+            }
+            else {
+                this.items.push(bagel[i]);
+                this.total++;
+                
+            }
+           
+        }
+        
+    
         // return the array
         return this.items;
     }
 
-    remove(item) {
+    remove(bagel) {
+        for (let i = 0; i < bagel.length; i++) {
+            if (!this.compareToList(bagel[i])) { 
+                return `${bagel[i]} cannot be removed, it is not in the basket`
+            }
+        }
         //find the item in the array
-        const index = this.items.indexOf(item);
+        const index = this.items.indexOf(bagel);
         //remove it
         this.items.splice(index, 1);
         //return the array
         return this.items;
     }
+
+    basketCapacity() {
+        if (this.total >= this.capacity){
+            return true
+        } 
+        else { return false 
+        }
+    }
+
+    compareToList(bagel) {
+        if (this.items.includes(bagel)) {
+            return true
+        }
+        else {
+            return false
+        }
+
+    }
+
+    increaseCapacity(bb) {
+        
+        if (this.total <= 50) {
+            return this.capacity += bb
+            // return "basket increased"
+        } 
+        else {
+            return "max limit 50"
+        }
+    }
+        
+       
 }
 module.exports = Basket
